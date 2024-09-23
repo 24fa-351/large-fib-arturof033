0d54be6cfe96d4c3429483b20f837dc3b9676f1f
arturof033 <68297760+arturof033@users.noreply.github.com>
2024-09-22 20:37:46 -0700

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,7 +1,55 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+#define MAX_LEN 10
+
+unsigned long long iterativeFibonacci(unsigned long long n)
+{
+    unsigned long long result = 0;
+    unsigned long long value1 = 0;
+    unsigned long long value2 = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        result = value1 + value2;
+        value2 = value1;
+        value1 = result; 
+    }
+
+    return result;
+}
+
+unsigned long long recursiveFibonacci(unsigned long long n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    if (n == 1)
+    {
+        return 1;
+    }
+    
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+}
+
+int main(int argc, char * argv[]) {
+    
+    unsigned long long  N = atoi(argv[1]) - 1;
+
+    if (*argv[2] == 'i')
+    {
+        printf("%llu", iterativeFibonacci(N));
+        printf("\n");
+    }
+
+    if (*argv[2] == 'r')
+    {
+        printf("%llu", recursiveFibonacci(N));
+        printf("\n");
+    }
+
    return 0;
 }
+
+
